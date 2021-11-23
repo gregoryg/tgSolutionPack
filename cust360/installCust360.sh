6f423ff4bcf8028f7e99023e93327935ecca17e5
rhardaway <robert.hardaway@gmail.com>
2021-11-23 07:09:41 -0700

    cust360

---
+++ cust360/installCust360.sh
@@ -0,0 +1,6 @@
+gsql ./cust360/scripts/create_cust360_schema.gsql
+gsql ./cust360/scripts/customer360_load_er.gsql
+gsql ./cust360/scripts/customer360_load_camp.gsql
+gsql ./cust360/scripts/customer360_load_events.gsql
+gsql ./cust360/scripts/customer360_load_orders.gsql
+gsql ./cust360/scripts/customer360_load_ratings.gsql
