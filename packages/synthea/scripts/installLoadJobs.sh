0e075f91abb6b8ee41eb7718b06e8dfdfdd61212
rhardaway <robert.hardaway@gmail.com>
2022-01-10 20:57:32 -0700

    synthes updates

--- packages/synthea/scripts/installLoadJobs.sh
+++ packages/synthea/scripts/installLoadJobs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-gsql < ./synthea/scripts/loadOrganizations.gsql
-gsql < ./synthea/scripts/loadEncounters.gsql
-gsql < ./synthea/scripts/loadPatientSymptoms.gsql
-gsql < ./synthea/scripts/loadImaging.gsql
-gsql < ./synthea/scripts/loadPayerTransitions.gsql
-gsql < ./synthea/scripts/loadImmunizations.gsql
-gsql < ./synthea/scripts/loadPayers.gsql
-gsql < ./synthea/scripts/loadProcedures.gsql
-gsql < ./synthea/scripts/loadAllergies.gsql
-gsql < ./synthea/scripts/loadMedications.gsql
-gsql < ./synthea/scripts/loadProviders.gsql
-gsql < ./synthea/scripts/loadAttributes.gsql
-gsql < ./synthea/scripts/loadObservations.gsql
-gsql < ./synthea/scripts/loadZips.gsql
-gsql < ./synthea/scripts/loadCareplans.gsql
-gsql < ./synthea/scripts/loadOrganizations.gsql
-gsql < ./synthea/scripts/loadConditions.gsql
-gsql < ./synthea/scripts/loadPatient.gsql
-gsql < ./synthea/scripts/loadDevices.gsql
+gsql < ./packages/synthea/scripts/loadOrganizations.gsql
+gsql < ./packages/synthea/scripts/loadEncounters.gsql
+gsql < ./packages/synthea/scripts/loadPatientSymptoms.gsql
+gsql < ./packages/synthea/scripts/loadImaging.gsql
+gsql < ./packages/synthea/scripts/loadPayerTransitions.gsql
+gsql < ./packages/synthea/scripts/loadImmunizations.gsql
+gsql < ./packages/synthea/scripts/loadPayers.gsql
+gsql < ./packages/synthea/scripts/loadProcedures.gsql
+gsql < ./packages/synthea/scripts/loadAllergies.gsql
+gsql < ./packages/synthea/scripts/loadMedications.gsql
+gsql < ./packages/synthea/scripts/loadProviders.gsql
+gsql < ./packages/synthea/scripts/loadAttributes.gsql
+gsql < ./packages/synthea/scripts/loadObservations.gsql
+gsql < ./packages/synthea/scripts/loadZips.gsql
+gsql < ./packages/synthea/scripts/loadCareplans.gsql
+gsql < ./packages/synthea/scripts/loadOrganizations.gsql
+gsql < ./packages/synthea/scripts/loadConditions.gsql
+gsql < ./packages/synthea/scripts/loadPatient.gsql
+gsql < ./packages/synthea/scripts/loadDevices.gsql
 
-## gsql < ./synthea/scripts/loadLocations.gsql
-## gsql < ./synthea/scripts/loadPatientNotes.gsql
+## gsql < ./packages/synthea/scripts/loadLocations.gsql
+## gsql < ./packages/synthea/scripts/loadPatientNotes.gsql
