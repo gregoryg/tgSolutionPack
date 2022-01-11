76052ce85e258dde0aa540cbcda55810b49926d2
rhardaway <robert.hardaway@gmail.com>
2022-01-11 14:50:52 -0700

    cust360 paths

--- packages/cust360/installCust360.sh
+++ packages/cust360/installCust360.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 gsql ./packages/cust360/scripts/create_cust360_schema.gsql
-gsql ./packages/cust360/scripts/create_cust360_schema.gsql
 gsql ./packages/cust360/scripts/customer360_load_er.gsql
 gsql ./packages/cust360/scripts/customer360_load_camp.gsql
 gsql ./packages/cust360/scripts/customer360_load_events.gsql
